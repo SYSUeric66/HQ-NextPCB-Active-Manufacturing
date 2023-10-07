e2b06d3a65498fe625f05390fa2674ec6128819b
yihuo <yihuo0420@163.com>
2023-10-07 14:22:14 +0800

    Remove dist

--- kicad_amf_plugin/event/locale_change.py
+++
@@ -1,5 +0,0 @@
-import wx.lib.newevent as ne
-
-LocaleChangeEvent, EVT_LOCALE_CHANGE = ne.NewCommandEvent()
-
-
--- kicad_amf_plugin/settings/setting_manager.py
+++ kicad_amf_plugin/settings/setting_manager.py
@@ -1,10 +1,20 @@
 
-class _SettingManager :
+import wx
+
+class _SettingManager(wx.EvtHandler) :
     def __init__(self) -> None:
+        self.app : wx.App = None
+
+    def set_language( self, lang : int):
+        self
         pass
 
+    def get_language(self, lang : int):
+        self
+        pass
 
-
+    def register_app(self, app : wx.App):
+        self.app = app
 
 
 
