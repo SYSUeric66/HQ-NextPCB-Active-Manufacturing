a5d09a0539cf8b93b10b003edef6488d805e2280
liangtie.qian <yihuo0420@163.com>
2023-10-17 18:05:23 +0800

    Update the pcm script

--- PCM/create_pcm_archive.sh
+++ PCM/create_pcm_archive.sh
@@ -2,15 +2,16 @@
 
 # heavily inspired by https://github.com/4ms/4ms-kicad-lib/blob/master/PCM/make_archive.sh
 
+VERSION=$1
 PRJECT_ROOT=`pwd`
 PCM_ROOT="$PRJECT_ROOT/PCM"
 TRANSLATION_PATH="$PRJECT_ROOT/kicad_amf_plugin/language"
 ACHIEVE_PATH="$PRJECT_ROOT/PCM/archive"
 PLUGIN_PATH="$ACHIEVE_PATH/plugins"
 RESOURCE_PATH="$ACHIEVE_PATH/resources"
+OUTPUT_ZIP_PATH="$PCM_ROOT/KiCAD-PCM-$VERSION.zip"
 
 
-VERSION=$1
 
 echo "Clean up old files"
 rm -f $PCM_ROOT/*.zip
@@ -51,11 +52,11 @@ sed -i "/INSTALL_SIZE_HERE/d" $META_DATA_PATH
 
 echo "Zip PCM archive"
 cd $ACHIEVE_PATH
-zip -r $PCM_ROOT/KiCAD-PCM-$VERSION.zip .
+zip -r $OUTPUT_ZIP_PATH .
 
 echo "Gather data for repo rebuild"
 echo VERSION=$VERSION >> $GITHUB_ENV
-echo DOWNLOAD_SHA256=$(shasum --algorithm 256 PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f1) >> $GITHUB_ENV
-echo DOWNLOAD_SIZE=$(ls -l PCM/KiCAD-PCM-$VERSION.zip | xargs | cut -d' ' -f5) >> $GITHUB_ENV
+echo DOWNLOAD_SHA256=$(shasum --algorithm 256 $OUTPUT_ZIP_PATH | xargs | cut -d' ' -f1) >> $GITHUB_ENV
+echo DOWNLOAD_SIZE=$(ls -l $OUTPUT_ZIP_PATH | xargs | cut -d' ' -f5) >> $GITHUB_ENV
 echo DOWNLOAD_URL="https:\/\/github.com\/Bouni\/kicad-jlcpcb-tools\/releases\/download\/$VERSION\/KiCAD-PCM-$VERSION.zip" >> $GITHUB_ENV
-echo INSTALL_SIZE=$(unzip -l PCM/KiCAD-PCM-$VERSION.zip | tail -1 | xargs | cut -d' ' -f1) >> $GITHUB_ENV
+echo INSTALL_SIZE=$(unzip -l $OUTPUT_ZIP_PATH | tail -1 | xargs | cut -d' ' -f1) >> $GITHUB_ENV
